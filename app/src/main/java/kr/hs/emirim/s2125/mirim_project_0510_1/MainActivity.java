305b38aadda67fcbf4c89b4f3ba75ccecbe90486
seungchae1 <s2125@e-mirim.hs.kr>
2022-05-10 19:14:54 +0900

    Initial commit

---
+++ app/src/main/java/kr/hs/emirim/s2125/mirim_project_0510_1/MainActivity.java
@@ -0,0 +1,53 @@
+package kr.hs.emirim.s2125.mirim_project_0510_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+    EditText edit;
+    TextView text;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        LinearLayout baseLayout = new LinearLayout(this);
+        baseLayout.setOrientation(LinearLayout.VERTICAL);
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+        setContentView(baseLayout, params);
+
+        edit = new EditText(this);
+        Button btn = new Button(this);
+        text = new TextView(this);
+        edit.setTextSize(20);
+        btn.setTextColor(Color.BLACK);
+        btn.setBackgroundColor(Color.YELLOW);
+        btn.setText("버튼입니다");
+        text.setTextSize(25);
+        text.setTextColor(Color.MAGENTA);
+        text.setTypeface(Typeface.SANS_SERIF, Typeface.BOLD);
+
+        baseLayout.addView(edit);
+        baseLayout.addView(btn);
+        baseLayout.addView(text);
+
+        //EditText에 입력한 후 버튼을 클릭하면 TextView에 똑같은 문자열이 출력되게
+
+        btn.setOnClickListener(btnlistener);
+    }
+    View.OnClickListener btnlistener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            text.setText(edit.getText().toString()); //getText가 반환하는 것은 Editable형이기 때문에 String으로 변환해줘야함
+        }
+    };
+}
